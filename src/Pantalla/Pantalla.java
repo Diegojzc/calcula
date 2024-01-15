93e72f30b0360c866a876ab510d5d1fd2467e2ff
Diegojzc <djavi91_05@hotmail.com>
2024-01-15 18:15:45 +0100

    inicio

---
+++ src/Pantalla/Pantalla.java
@@ -0,0 +1,23 @@
+package Pantalla;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Pantalla {
+    public JPanel panel;
+    public JTextField textField1;
+    public JTextField textField2;
+    public JTextField textField3;
+    public JButton button1;
+    public JButton button2;
+
+
+
+
+    }
+
+
+
+
+
---
+++ src/modelo/Principal.java
@@ -0,0 +1,10 @@
+package modelo;
+
+import Pantalla.Pantalla;
+
+public class Principal {
+    public static void main(String []args){
+        Pantalla pan = new Pantalla();
+       // pan.setVisible(true);
+    }
+}
